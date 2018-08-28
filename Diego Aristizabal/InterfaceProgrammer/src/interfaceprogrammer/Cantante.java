0e7cdda3826a9b922b6cc63700738e0f7e35fef8
Diego Aristizabal <diegoarm2394@hotmail.com>
2018-08-27 21:05:36 -0500

    Diego Aristizabal

---
+++ Diego Aristizabal/InterfaceProgrammer/src/interfaceprogrammer/Cantante.java
@@ -0,0 +1,17 @@
+package interfaceprogrammer;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public interface Cantante {
+    
+    public void cantar();
+    
+}
---
+++ Diego Aristizabal/Programmer/src/java/Implementacion/Interface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Implementacion;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+
+
+// Declaro la clase interface para ser implemetada
+public interface Interface {
+    public void Ejemplo ();
+    
+}
