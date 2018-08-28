af2e9e680b70fc4da3169fa8e1b2fb4963581657
Ximena Valero <xvalerogo@uninpahu.edu.co>
2018-08-27 21:13:53 -0500

    Taller de interfaz

---
+++ Valero Ximena/TallerInterfaz/src/tallerinterfaz/Cantante.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tallerinterfaz;
+
+/**
+ *
+ * @author Ximena
+ */
+public interface Cantante {
+    
+    String nombre = "";
+    int edad = 0;
+    
+    public void cantar();
+    public void informacion();    
+}
