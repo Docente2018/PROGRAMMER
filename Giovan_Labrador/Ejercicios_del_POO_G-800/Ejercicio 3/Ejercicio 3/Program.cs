0ee28ba3018fadbe21538eb0b576f1598c38293f
giovan <Giovan>
2018-08-27 21:24:46 -0500

    hola2

---
+++ Giovan_Labrador/Ejercicios_del_POO_G-800/Ejercicio 3/Ejercicio 3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System
+
+namespace Ejercicio_3
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            TrasporteA trasporte = new TrasporteA();
+
+        }
+    }
+}
---
+++ Giovan_Labrador/Ejercicios_del_POO_G-800/ejercicio POO/ConsoleApp1/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     public class  Animal
+    {
+       public void animal()
+        {
+            Console.WriteLine("Ha nacido un animal ");
+        }
+
+        public void rugir()
+        {
+          Console.WriteLine("Hace algun ruido ");
+        }
+    }
+}
