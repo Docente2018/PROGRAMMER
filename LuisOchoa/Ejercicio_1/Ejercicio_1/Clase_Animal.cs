5899880d17091a98ba3200bc5fed553c07126265
LuisOchoa <42757672+lochoabu01@users.noreply.github.com>
2018-08-29 05:52:59 -0500

    Ejercicio Primera Clase

---
+++ LuisOchoa/Ejercicio_1/Ejercicio_1/Clase_Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+    public class Clase_Animal
+    {
+        static void Main(string[] args)
+        {
+            Accion animal = new Accion();
+            animal.nace();
+            animal.rugir();
+        }
+    }
+
+    public class Animal
+    {
+        public void nace()
+        {
+            Console.WriteLine("Ha nacido un animal");
+            Console.ReadKey();
+        }
+
+        public void rugir()
+        {
+            Console.WriteLine("Hace algun ruido");
+            Console.ReadKey();
+        }  
+    }
+
+    public class Accion : Animal
+    { }
+}
