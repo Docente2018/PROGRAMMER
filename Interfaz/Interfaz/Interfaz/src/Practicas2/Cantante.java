ed285ef5cc46a27ef5402577dd04397ff1daa6f1
jorge2509 <jorge-l_08@outlook.com>
2018-08-30 19:41:12 -0500

    Creacion de rama

---
+++ Interfaz/Interfaz/Interfaz/src/Practicas2/Cantante.java
@@ -0,0 +1,16 @@
+
+// en las interfaces no existen los atributos
+//todos sus metodos son abstractos y no se implementan
+//no es una clase es interfaz
+// no se hereda se implementa
+//no se puede instanciar una interfaz
+// no hay constructores en una interfaz
+package Practicas2;
+
+
+public interface Cantante {
+    
+    public void cantar();
+    
+    public String escuchar();
+}
