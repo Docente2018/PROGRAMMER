25a3456cb0395afb33c830da748be35b422a0bba
gio1319 <42757591+gio1319@users.noreply.github.com>
2018-09-10 18:14:06 -0500

    Add files via upload

---
+++ Giovan_Labrador/PruebaCorte1/PruebaCorte1/Cuadrado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaCorte1
+{
+     public class Cuadrado
+    {
+        
+        public int Lado1;
+        public int Lado2;
+        public int Lado3;
+        public int Lado4;
+        public int Res2;
+        public int Res3;
+    }
+}
