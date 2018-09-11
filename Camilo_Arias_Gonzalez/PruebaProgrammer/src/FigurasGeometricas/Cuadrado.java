95312ae049686c264023b5576576de046c33fe2c
soydevcag <ariasgonzalezcamilo@gmail.com>
2018-09-10 20:19:25 -0500

    programmer

---
+++ Camilo_Arias_Gonzalez/PruebaProgrammer/src/FigurasGeometricas/Cuadrado.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FigurasGeometricas;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Cuadrado extends Figura {
+    private int ladoCuad;
+
+    public Cuadrado(int ladoCuad) {
+        this.ladoCuad = ladoCuad;
+    }
+    
+    public Cuadrado() {
+    }
+
+    public int getLadoCuad() {
+        return ladoCuad;
+    }
+    
+
+}
