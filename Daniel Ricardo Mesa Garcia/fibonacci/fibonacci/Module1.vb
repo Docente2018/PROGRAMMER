ac1e27ce8dc06d4a514a2596a9d50e08612480bc
DRMG26 <daniemayo26@gmail.com>
2018-09-17 21:45:12 -0500

    Subiendo fibonacci

--- Daniel Ricardo Mesa Garcia/fibonacci/fibonacci/Module1.vb
+++ Daniel Ricardo Mesa Garcia/fibonacci/fibonacci/Module1.vb
@@ -1,4 +1,6 @@
 ﻿Module Module1
+
+    'Se crea funcion fibonacci en cual recibe la cantidad de numeros a mostrar de la seria fibonacci
     Sub fibonacci(cantidad As Integer)
         Dim A As Integer = 1
         Dim B As Integer
@@ -15,9 +17,9 @@
 
     End Sub
     Sub Main()
-        Dim cantidad
-        Dim cont As Integer
-        Dim a As Boolean = True
+        Dim cantidad 'Almacena la cantidad de numeros de la serie de fibonacci a mostrar
+        Dim cont As Integer 'contador en cual es usado para los intentos que le quedan al usuario
+        Dim a As Boolean = True 'Almacena True para repetir sin fin el bucle while, si llegara ser false se terminara el bucle
         Console.WriteLine("Digita la cantidad de numeros a mostrar de la serie de fibonacci")
         While a = True
             Try
@@ -25,8 +27,9 @@
                 Console.WriteLine("Digita un numero:")
                 cantidad = Console.ReadLine
                 Console.WriteLine("")
+                'si es numerico, mayor a 0 y menor a 47 entrara a la funcion fibonacci
                 If IsNumeric(cantidad) And cantidad > 0 And cantidad < 47 Then
-                    fibonacci(cantidad)
+                    fibonacci(cantidad) 'se llama la funcion enviado el valor
                 ElseIf cantidad >= 47 Then
                     cont = cont + 1
                     Console.WriteLine("Excediste el maximo")
@@ -40,6 +43,7 @@
                 Console.WriteLine("Te quedad " & 3 - cont & " Intento")
             End Try
 
+            'si el contador llegara ser igual a 3 se acaba el bucle while
             If cont = 3 Then
                 a = False
                 Console.WriteLine("Agotaste tu cantidad de intentos")
