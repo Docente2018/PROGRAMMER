2845246c372113f0fa00e19d1b008fd8fe522a6c
gio1319 <42757591+gio1319@users.noreply.github.com>
2018-09-24 21:09:22 -0500

    Add files via upload

---
+++ Giovan_Labrador/torrede hanoy/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace torrede_hanoy
+{
+    class Program
+    {
+
+        static void torreHanoi(int n, int o, int d, int aux) // se declaran las variables
+        {
+
+
+            if (n > 0) // si la variable de captura es mayor a cero se inicia el programa
+            {
+                torreHanoi(n - 1, o, aux, d);
+                Console.Write(" se meuve anillo desde torre= " + o + " hasta torre= " + d);
+                torreHanoi(n - 1, aux, d, o);
+
+            }
+        }
+
+
+
+        static void Main(string[] args)// metodo para captura la cantidad de anillos 
+        {
+            int n;
+            Console.Write("Favor ingresar la cantidad de anillos");
+            n = int.Parse(Console.ReadLine());
+            torreHanoi(n, 1, 3, 2);
+            Console.ReadKey();
+
+        
+                  
+        
+      }
+    }
+}
+    
+    
+    
+
+
+
+ 
+        
+    
+
+
