3d23f750481bc98c9615da77f2dc07303d9fafa9
Arley Antonio Cabrera Mendoza <acabreme@everis.com>
2018-10-18 19:49:44 -0500

    ejercicio de ordenamiento

---
+++ Arley_Cabrera/ejercicioOrdenamiento/ejercicioOrdenamiento/MetodoPorQuickShort.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioOrdenamiento
+{
+    public class MetodoPorQuickShort
+    {
+        int h;
+        int[] vector;
+
+        public void llenar(int n)
+        {
+            h = n;
+
+            Random numAleatorio = new Random();
+            vector = new int[h];
+
+            for (int i = 0; i < h; i++)
+            {
+                int numero = numAleatorio.Next(1, h);
+
+                Console.Write("ingrese valor {0} \n: ", i + 1 + ": " +  numero);
+                vector[i] = Convert.ToInt32(numero);
+            }
+            quicksort(vector, 0, h - 1);
+             mostrar();
+        }
+
+        public void quicksort(int[] vector, int primero, int ultimo)
+        {
+            int i, j, central;
+            double pivote;
+            central = (primero + ultimo) / 2;
+            pivote = vector[central];
+            i = primero;
+            j = ultimo;
+            do
+            {
+                while (vector[i] < pivote) i++;
+                while (vector[j] > pivote) j--;
+                if (i <= j)
+                {
+                    int temp;
+                    temp = vector[i];
+                    vector[i] = vector[j];
+                    vector[j] = temp;
+                    i++;
+                    j--;
+                }
+            } while (i <= j);
+
+            if (primero < j)
+            {
+                quicksort(vector, primero, j);
+            }
+            if (i < ultimo)
+            {
+                quicksort(vector, i, ultimo);
+            }
+        }
+
+        private void mostrar()
+        {
+            Console.WriteLine("Vector ordenados en forma ascendente");
+            for (int i = 0; i < h; i++)
+            {
+                Console.Write("{0} ", vector[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Arley_Cabrera/ejercicioOrdenamiento/ejercicioOrdenamiento/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioOrdenamiento
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Seleccione el metodo de ordenamiento \n");
+
+                Console.WriteLine("Escriba (1) para metodo por Sort, (2) para metodo por Quick Short, (3) metodo por burbuja \n");
+
+                try
+                {
+                    int num = int.Parse(Console.ReadLine());
+                    switch (num)
+                    {
+                        case 1:
+                            ordenarPorMetodoSort();
+                            break;
+
+                        case 2:
+                            MetodoPorQuickShort metodoQuick = new MetodoPorQuickShort();
+                            int n;
+                            Console.WriteLine("Metodo de Quick Sort");
+                            Console.Write("Cuantos longitud del vector: ");
+                            n = Int32.Parse(Console.ReadLine());
+
+                            metodoQuick.llenar(n);
+
+                            break;
+
+                        case 3:
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Ingrese solo valores númericos \n");
+                }
+                
+            }
+
+
+        }
+
+        public static void ordenarPorMetodoSort()
+        {
+            //ordenamiento por el primer metodo
+            Console.WriteLine("Ingrese 10 Números en desorden \n");
+            //Creamos un vector (Array) de enteros con una longitud de 10 elementos
+            int[] numeros = new int[10];
+
+
+            //Solicitamos al usuario los 10 valores (desordenados)
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Ingrese un número:");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            //Ordenamos la lista e imprimimos los valores.
+            Array.Sort(numeros);
+
+            Console.WriteLine("\nLos números ordenados son:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+
+            //Para mantener la consola abierta
+            Console.ReadKey();
+        }
+
+        public static void ordenarNumeros(int[] numerosDesorden)
+        {
+            
+            
+
+           
+        }
+    }
+}
