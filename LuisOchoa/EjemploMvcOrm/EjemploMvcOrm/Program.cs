e81a67ee11b9f9185e73d1b1dfb0b567430413fd
LuisOchoa <42757672+lochoabu01@users.noreply.github.com>
2018-10-29 21:33:31 -0500

    Add files via upload

---
+++ LuisOchoa/EjemploMvcOrm/EjemploMvcOrm/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploOrmLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio aplicando ORM - LINQ");
+
+            string[] arreglo = { "Paco","Hugo","Luis" };
+
+            Console.WriteLine(string.Join(",", arreglo));
+
+            var nombresOrdenados =
+                from num in arreglo
+                orderby num ascending
+                select num;
+
+            Console.WriteLine("Ordenando...");
+            foreach (string n in nombresOrdenados)
+            {
+                Console.WriteLine("{0}", n);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
